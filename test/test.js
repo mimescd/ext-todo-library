8d69f04d879a41295d6a6c33156c59448467a77b
Curtis Mimes <mimeswebdev@gmail.com>
2018-01-13 12:21:33 -0500

    added 'it should append html and raw text correctly' test

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ describe("TodoItem basics", () => {
     it("should be instantiated correctly", () => {
     	let testText = "some todo text";
     	let todo = new TodoItem(testText);
+    	
     	assert.equal(todo.getRawText(), testText);
     	assert.equal(todo.getDisplayHtml(), "<div>" + testText + "</div>");
     });
@@ -13,9 +14,23 @@ describe("TodoItem basics", () => {
     	let newTestText = "this some new shit now b";
     	let newHtml = "<div>aw fuck ye</div>";
     	let todo = new TodoItem(testText);
+    	
     	todo.setRawText(newTestText);
     	todo.setDisplayHtml(newHtml);
     	assert.equal(todo.getRawText(), newTestText);
     	assert.equal(todo.getDisplayHtml(), newHtml);
+    });
+    it("should append html and raw text correctly", ()=>{
+    	let testText = "some todo text";
+    	let newTestText = "this some new shit now b";
+    	let newHtml = "<div>aw fuck ye</div>";
+    	let todo = new TodoItem(testText);
+    	let currentHtml = todo.getDisplayHtml();
+    	
+    	todo.appendRawText(newTestText);
+    	todo.appendDisplayHtml(newHtml);
+    	assert.equal(todo.getRawText(), testText+newTestText);
+    	assert.equal(todo.getDisplayHtml(), currentHtml+newHtml);
+
     });
 });
